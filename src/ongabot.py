263e8992e0f8e520f640ea062fdb5fec1cbd450e
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2018-11-15 23:12:46 +0100

    [ongabot] Remove temp stuff and then heavy WIP

--- src/ongabot.py
+++ src/ongabot.py
@@ -17,56 +17,55 @@ def error(bot, update, error):
   """Log Errors caused by Updates."""
   logger.warning('Update "%s" caused error "%s"', update, error)
 
+# TODO: Extract commands into their own handlers and files
+def startCommand(bot, update):
+  """Print the help text for a /start or /help command"""
+  helpText = "Welcome traveler, my name is ONGAbot.\n" \
+             "I'm the one and only, the truth speaker.\n" \
+             "\n" \
+             "My duties are:\n" \
+             " - Uphold the law, obviously, with weekly ON/GA polls\n" \
+             " - Give praise to the faithful\n" \
+             " - Aid the needing\n" \
+             " - Condemn the wicked\n" \
+             "\n" \
+             "Commandments:\n" \
+             "/help - To show this very helpful text\n" \
+             "/onga - Print the image of the one\n" \
+             "/newevent - Create a new event, args=TBD\n"
 
-def start(bot, update):
-  update.message.reply_text('All hail ON/GA')
+  update.message.reply_text(helpText)
 
 
 def onga(bot, update):
+  """Print the image of the one when the true word of /onga is spoken"""
   with open('onga.jpg', 'rb') as photo:
-    update.message.reply_photo(photo, 'ON/GA is here 😺')
+    update.message.reply_photo(photo, 'All are naked in front of the ONE!')
 
+# TODO:
+# [] Print message similar to other poll bots inc. options
+# [] Add answer buttons
+# [] Connect answer buttons to data
+# [] Update original message with updated data
+# [] Add status command to reprint active poll
+# [] Cancel/close command
+# [] Basic error handling if already exist a poll
+# [] Add end time for a poll (auto close)
+# [] Decide creation of poll (manual vs. automated)
+# [] Persistent storage for current poll
+# [] Multiple active polls
+# [] Add custom polls (other than CS events)
+# [] Add Configurable or random answer options
+def newevent(bot, update):
+  """Create a poll as result of command /newevent"""
+  titleText = "ON/GA time, 21/11 kl 17.30"
+  infoText = "Insert priority here"
 
-def alarm(bot, job):
-  """Send the alarm message."""
-  bot.send_message(job.context, text='Beep!')
-
-
-def set_timer(bot, update, args, job_queue, chat_data):
-  """Add a job to the queue."""
-  chat_id = update.message.chat_id
-  try:
-    # args[0] should contain the time for the timer in seconds
-    due = int(args[0])
-    if due < 0:
-      update.message.reply_text('Sorry we can not go back to future!')
-      return
-
-    # Add job to queue
-    job = job_queue.run_once(alarm, due, context=chat_id)
-    chat_data['job'] = job
-
-    update.message.reply_text('Timer successfully set!')
-
-  except (IndexError, ValueError):
-    update.message.reply_text('Usage: /set <seconds>')
-
-
-def unset(bot, update, chat_data):
-  """Remove the job if the user changed their mind."""
-  if 'job' not in chat_data:
-    update.message.reply_text('You have no active timer')
-    return
-
-  job = chat_data['job']
-  job.schedule_removal()
-  del chat_data['job']
-
-  update.message.reply_text('Timer successfully unset!')
-
+  message = '{}\n{}'.format(titleText, infoText)
+  update.message.reply_text(message)
 
 def main():
-  """Run bot."""
+  """Setup and run ONGAbot"""
   updater = Updater(API_TOKEN)
 
   # Get the dispatcher to register handlers
@@ -76,12 +75,7 @@ def main():
   dp.add_handler(CommandHandler("start", start))
   dp.add_handler(CommandHandler("help", start))
   dp.add_handler(CommandHandler("onga", onga))
-  dp.add_handler(CommandHandler("set", set_timer,
-                                pass_args=True,
-                                pass_job_queue=True,
-                                pass_chat_data=True))
-  dp.add_handler(CommandHandler("unset", unset,
-                                pass_chat_data=True))
+  dp.add_handler(CommandHandler("newevent", newevent))
 
   # log all errors
   dp.add_error_handler(error)
