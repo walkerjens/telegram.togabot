64e83d6ec2e857ae0eab386ef2cf8af8977686fc
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2021-03-15 14:09:28 +0100

    [misc] Add __version__ and autotagger action (#34)
    Fixes #16

---
+++ ongabot/__init__.py
@@ -0,0 +1,3 @@
+"""The one and only ON/GA Telegram bot."""
+
+from ._version import __version__  # noqa: F401
---
+++ ongabot/_version.py
@@ -0,0 +1,3 @@
+"""The module defines the version(s) of ONGAbot."""
+
+__version__ = "0.1.0"
